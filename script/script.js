cae96c7c80394ce38edc2f705429027e413bd47a
Eldar Akavov <akavoveldar3005@gmail.com>
2020-08-26 01:21:55 +0300

    fix(lesson06):script.js

--- script/script.js
+++ script/script.js
@@ -4,28 +4,26 @@ const isNumber = function(n){
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-const goal = 58;
-confirm('Я хочу сыграть с тобой в игру');
 function game(){
-  let question = +prompt('Угадай число от 1 до 100');
-  function checkAnswer(){
-    switch(true){
-      case !isNumber(question): alert('Введи число!');
-      return game();
-      case question === goal: alert('Вы угадали!');
-        const answer = confirm('игру завершаем?');
-        if(answer === true){
-          return null;
-        }else{
-          game();
-        }
-        break;
-      case question <= 50: alert('Загаданное число больше');
-      return game();
-      case question > 50: alert('Загаданное число меньше');
-      return game();
+  const goal = 58;
+  
+   function checkAnswer(){
+    let question = prompt('Угадай число от 1 до 100');
+    if(isNumber(question)){
+        if(+question === goal){
+          if(confirm('Вы угадали!Игру повторяем?')){
+            checkAnswer();
+          }}else if(+question < goal){
+            alert('Загаданное число меньше');
+            checkAnswer();
+          }else if(+question > goal){
+            alert('Загаданное число больше');
+            checkAnswer();
+          }
+    } else{
+      alert('Введи число!');
+      return checkAnswer();
     }
-  return question;
   }
   checkAnswer();
 }
